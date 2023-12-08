6d1b5a2b3502e251e8ac3f2893ed34b9410e99c1
yssh-raj10 <yashraj.se10@gmail.com>
2023-12-08 23:58:10 +0530

    forgot to push lol

--- MySql-BookStore/pkg/controllers/book-controller.go
+++ MySql-BookStore/pkg/controllers/book-controller.go
@@ -1,1 +1,45 @@
 package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/yash-raj10/GO-Lang-Prjs/MySql-BookStore/pkg/models"
+	"github.com/yash-raj10/GO-Lang-Prjs/MySql-BookStore/pkg/utils"
+	"net/http"
+	"strconv"
+)
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	//get a book stru from models and also we receive req in form of r
+	CreateBook := &models.Book{}
+	// put book stru and r and unmartial it to a data which database can understand
+	utils.ParseBody(r, CreateBook)
+	b := CreateBook.CreateBook()
+	res, _ := json.Marshal(b)
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	newBooks := models.GetAllBooks()
+	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	bookDetails, _ := models.GetBookById(ID)
+
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
