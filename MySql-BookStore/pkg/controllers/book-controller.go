082da70167b6ec15af95bd40b78f7a10bccfa094
yssh-raj10 <yashraj.se10@gmail.com>
2023-12-10 02:33:03 +0530

    done

--- MySql-BookStore/pkg/controllers/book-controller.go
+++ MySql-BookStore/pkg/controllers/book-controller.go
@@ -43,3 +43,43 @@ func GetBookById(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println(err)
+	}
+	book := models.DeleteBook(ID)
+	res, _ := json.Marshal(book)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	var updatebook = &models.Book{}
+	utils.ParseBody(r, updatebook)
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println(err)
+	}
+	bookDetails, db := models.GetBookById(ID)
+	if updatebook.Name != "" {
+		bookDetails.Name = updatebook.Name
+	}
+	if updatebook.Author != "" {
+		bookDetails.Author = updatebook.Author
+	}
+	if updatebook.Publication != "" {
+		bookDetails.Publication = updatebook.Publication
+	}
+	db.Save(&bookDetails)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
--- MySql-BookStore/pkg/routes/bookstore-routes.go
+++ MySql-BookStore/pkg/routes/bookstore-routes.go
@@ -8,7 +8,7 @@ import (
 var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
 	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
-	router.HandleFunc("/book/{bookID}", controllers.GetBookId).Methods("GET")
+	router.HandleFunc("/book/{bookID}", controllers.GetBookById).Methods("GET")
 	router.HandleFunc("/book/{bookID}", controllers.UpdateBook).Methods("GET")
 	router.HandleFunc("/book/{bookID}", controllers.DeleteBook).Methods("DELETE")
 }
